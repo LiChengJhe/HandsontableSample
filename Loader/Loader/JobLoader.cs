9815f256653cd97632ec37e0d832e5fa62cd8f31
李承哲(ChengjheLi) <chengjheli@chipbond.com.tw>
2019-04-12 20:37:37 +0800

    add
    Signed-off-by: 李承哲(ChengjheLi) <chengjheli@chipbond.com.tw>

---
+++ Loader/Loader/JobLoader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loader
+{
+    public class JobProp
+    {
+        public string Path { get; set; }
+        public string JobName { get; set; }
+        public string TypeName { get; set; }
+        public string MethodName { get; set; }
+        public object[] Params { get; set; }
+    }
+    public class JobLoader
+    {
+        public static void Execute(JobProp prop)
+        {
+
+            AppDomainSetup appDomainSetup = new AppDomainSetup
+            {
+                ApplicationBase = System.Environment.CurrentDirectory,
+                 ShadowCopyDirectories=$"{System.Environment.CurrentDirectory}",
+                  ShadowCopyFiles=true.ToString().ToLower(),
+                  PrivateBinPath = "dll"
+            };
+
+            AppDomain appDomain = AppDomain.CreateDomain(prop.JobName, null, appDomainSetup);
+
+ 
+
+
+            AssemblyLoader assemblyLoader = (AssemblyLoader)appDomain.CreateInstanceAndUnwrap(typeof(AssemblyLoader).Assembly.FullName, typeof(AssemblyLoader).FullName);
+            Assembly assembly = assemblyLoader.GetAssembly(prop.Path);
+            Type assemblyType = assembly.GetType(prop.TypeName);
+            MethodInfo assemblyMethod = assemblyType.GetMethod(prop.MethodName);
+            var assemblyInstance = Activator.CreateInstance(assemblyType);
+            Console.WriteLine(assemblyMethod.Invoke(assemblyInstance, prop.Params));
+      
+            AppDomain.Unload(appDomain);
+ 
+        }
+    }
+    public class AssemblyLoader : MarshalByRefObject
+    {
+        public Assembly GetAssembly(string assemblyPath)
+        {
+            Assembly assembly =  Assembly.Load(File.ReadAllBytes(assemblyPath));
+            return assembly;
+
+        }
+    }
+}
---
+++ Loader/Loader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable.Interval(TimeSpan.FromSeconds(10)).Subscribe((i) =>
+            {
+                JobLoader.Execute(
+                     new JobProp
+                     {
+                         JobName = "Test",
+                         Path = $"C:\\Users\\ChengjheLi\\Desktop\\Loader\\Loader\\bin\\Debug\\dll\\Job.dll",
+                         MethodName = "Get",
+                         TypeName = "Job.Test"
+                     });
+     
+            });
+
+
+            Console.ReadKey();
+        }
+    }
+}
